5fe213857765853ba68911a6a3adfa32e77b2411
Akshat Rajvaidya <akshat.rajvaidya09@gmail.com>
2021-09-25 19:15:13 +0530

    Added a program to find the length of the String without using the length().

--- JavaInterview/src/main/java/com/qa/JavaInterviewPrograms/StringLength.java
+++ JavaInterview/src/main/java/com/qa/JavaInterviewPrograms/StringLength.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /*
- * Findthe length of the String without using the length()
+ * Find the length of the String without using the length()
  */
 
 public class StringLength {
